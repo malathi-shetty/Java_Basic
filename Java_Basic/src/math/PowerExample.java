435ce3fd76c8977e5122c3af947c15fbdb1f57dd
malathi <malathi@Deepak>
2025-02-23 12:31:29 +0530

    Power Example

---
+++ Java_Basic/src/math/PowerExample.java
@@ -0,0 +1,30 @@
+package math;
+
+public class PowerExample {
+
+	public static void main(String[] args) {
+		 double base = 2;
+	        double exponent = 3;
+	        
+	        double result = Math.pow(base, exponent);
+	        
+	        System.out.println("Result: " + result);  // Output will be 8.0
+	   
+
+	}
+
+}
+
+//In this example, 2 raised to the power of 3 results in 8.0. The Math.pow() method always returns a double.
+
+/*
+
+use the Math.pow() method to calculate the power of a number. The method takes two arguments: the base and the exponent. Here's the syntax:
+
+double result = Math.pow(base, exponent);
+
+base is the number you want to raise to a power.
+
+exponent is the power to which you want to raise the base.
+
+*/
